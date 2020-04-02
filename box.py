f75acf3f5ba92f49b33221bb088ff68426d8979c
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-02 12:58:42 -0400

    completed Print a Box exercise

---
+++ box.py
@@ -0,0 +1,11 @@
+# print a box based off user input for height and width
+# create variables for user input for width and height
+width = int(input("Width? "))
+height = int(input("Height? "))
+
+# adjust box dimensions based on user input
+box_width = ("*" * width)
+#have to offset width by 2 in order for dimensions to match
+box_height = ("*" + (" " * (width - 2)) + "*\n") #add newline here instead of final format string
+# display box
+print(f"{box_width}\n{(box_height * height)}{box_width}") 
