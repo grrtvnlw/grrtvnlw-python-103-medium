1df7115bb1f8d3eeb9348aee564c18d94f690276
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-02 14:46:17 -0400

    finished triangle exercise

--- triangle.py
+++ triangle.py
@@ -1,6 +1,6 @@
 # print a triangle 
 # set a variable for counter and user input
-# have to set at 1 for initial icon to appear
+# have to set counter to 1 for initial icon to appear
 counter = 1 
 inp = int(input("How tall should we make this thing? "))
 
