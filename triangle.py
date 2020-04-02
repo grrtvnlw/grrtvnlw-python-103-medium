51c5adfbd570dd38902b39513a9da2e2e22dbb9e
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-02 17:38:38 -0400

    refactored Triangle exercise

--- triangle.py
+++ triangle.py
@@ -1,12 +1,11 @@
 # print a triangle 
 # set a variable for counter and user input
-# have to set counter to 1 for initial icon to appear
-counter = 1 
+counter = 1 # set counter to 1 for no space above pyramid
 inp = int(input("How tall should we make this thing? "))
 
-while counter < inp:
+while counter <= inp:
     if counter == 1:
-        print((" " * (inp - counter)) + "*")
+        print(" " * (inp - counter) + "*")
     else:
         print((" " * (inp - counter)) + ("*" * counter) + ("*" * (counter - 1)))
     counter += 1
